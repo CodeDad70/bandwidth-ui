11730e22848f90b4e2bfed59c144beba9e251c9d
CodeDad <github email address>
2018-08-03 22:41:03 -0400

    moved NavCard to containers. It now has state and is longer a component. added a set state of showCard to be false on default. created a onClick that calls the showCard event. Added a ternary operator to the card div to show if showCard state is toggled to true

---
+++ src/containers/NavCard.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react' ;
+
+
+class NavCard extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      showCard: false,
+    }
+    this.showCard = this.showCard.bind(this);    
+  }
+
+  showCard(event) {
+    event.preventDefault()
+
+    this.setState({
+      showCard: true 
+    })
+  }
+
+  render() {
+    return (
+      <div>
+          <button onClick = {this.showCard}>
+            Services
+          </button>
+        
+        { this.state.showCard ? (
+          <div className="nav-card" >
+            <h3> Voice </h3>
+            <button>Inbound/Outbound</button>
+            <button>Toll-Free Voice</button>
+            <button>Voice API</button>
+            <button>SIP Trunking</button>
+          </div>
+          ) 
+          : (
+            null
+          )
+        }
+      </div>
+    );
+  }
+}
