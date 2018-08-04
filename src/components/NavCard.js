f1d100f295ec4e27e30dd4763f8b8b643ed93556
CodeDad <github email address>
2018-08-03 22:28:21 -0400

    created containers and component folders. created a navcard component with prelim nav elements

---
+++ src/components/NavCard.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react' ;
+
+
+class NavCard extends Component {
+  render() {
+    return (
+      <div>
+        <button>
+          Clients
+        </button>
+      </div> 
+    )
+  }
+}
